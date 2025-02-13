a961d9a2c656b8f577afda5cb2303293a6d9e7cf
meherab111 <mehrabborno@gmail.com>
2025-02-14 00:26:33 +0600

    published

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let clockElem = document.getElementById("clock");
+
+const funcTime = () => {
+
+    const clockValue = new Date();
+
+    clockElem.innerText = clockValue.toLocaleTimeString();
+
+};
+
+setInterval(funcTime, 1000);
