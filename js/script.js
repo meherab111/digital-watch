fa99d152b573c2f8d61cd3429a8edf8b81713aac
meherab111 <mehrabborno@gmail.com>
2025-02-25 02:08:34 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 let clockElem = document.getElementById("clock");
 
 const funcTime = () => {
+  const clockValue = new Date();
 
-    const clockValue = new Date();
-
-    clockElem.innerText = clockValue.toLocaleTimeString();
-
+  clockElem.innerText = clockValue.toLocaleTimeString();
 };
 
 setInterval(funcTime, 1000);
